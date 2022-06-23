dcc6527fdae95e58c27cdfdecfa1c658cc46be3a
joduckyong <dyddl7609@gmail.com>
2022-06-23 22:13:41 +0900

    데이터 전송목록 HTML 작업

--- src/main/java/kr/or/lx/server/TransmissionController.java
+++ src/main/java/kr/or/lx/server/TransmissionController.java
@@ -1,10 +1,19 @@
 package kr.or.lx.server;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.or.lx.common.ApiService;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -13,32 +22,36 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class TransmissionController {
 	
-	@GetMapping("/list")
-	public String transmissionList(ModelMap model) throws Exception{
-		log.info("transmissionList");
+    @Value("${agent.server.api.url}")
+    private String agentApiUrl;	
+   
+	@Autowired
+	private ApiService<?> apiService;
+	
+	@GetMapping("/send")
+	public String sendDataList(ModelMap model) throws Exception{
+		log.info("sendDataList");
 		
-		return "server/transmission/list";
+		return "server/transmission/send";
 	}
 	
-	@GetMapping("/history")
-	public String transmissionHistory(ModelMap model) throws Exception{
-		log.info("transmissionHistory");
+	@GetMapping("/reception")
+	public String receptionDataList(ModelMap model) throws Exception{
+		log.info("receptionDataList");
 		
-		return "server/transmission/history";
+		return "server/transmission/reception";
 	}
-	
-	@GetMapping("/add")
-	public String transmissionAdd(ModelMap model) throws Exception{
-		log.info("transmissionAdd");
+
+	@ResponseBody
+	@PostMapping("{apiId}")
+	public Object transmission(@RequestBody Map<String, Object> param, ModelMap model) throws Exception{
+		log.info("transmission");
 		
-		return "server/transmission/add";
-	}
-	
-	@GetMapping("/log")
-	public String transmissionLog(ModelMap model) throws Exception{
-		log.info("transmissionLog");
+		String url = agentApiUrl+param.get("url");
 		
-		return "server/transmission/log";
+		ResponseEntity<?> responseEntity = apiService.post(url, param);
+		Object object = responseEntity.getBody();
+		
+		return object;
 	}	
-	
 }
