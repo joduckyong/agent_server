dd621c002006f8c137d99f1e23b15b25444f2e86
joduckyong <dyddl7609@gmail.com>
2022-06-23 17:49:31 +0900

    first commit

---
+++ src/main/java/kr/or/lx/common/LoggerInterceptor.java
@@ -0,0 +1,33 @@
+package kr.or.lx.common;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.info(" Request URI \t:  " + request.getRequestURI());
+
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	}
+}
