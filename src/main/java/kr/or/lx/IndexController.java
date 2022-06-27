b843345bd25d0f48ea0e57928ba889131e0b0ae1
LAPTOP-AHDO2SSJ\kang <khighbig@gmai.com>
2022-06-27 22:10:20 +0900

    수신 관리 구현

--- src/main/java/kr/or/lx/IndexController.java
+++ src/main/java/kr/or/lx/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         
-        return "index";
+        return "server/receive/list";
     }
     
     @GetMapping("/posts/save")
