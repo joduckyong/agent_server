dc4149c6dc7c04aa23c0e3ef606c17404a9b5f46
LAPTOP-AHDO2SSJ\kang <khighbig@gmai.com>
2022-06-28 20:31:26 +0900

    url 명칭 수정

--- src/main/java/kr/or/lx/server/ReceiveController.java
+++ src/main/java/kr/or/lx/server/ReceiveController.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class ReceiveController {
 	
-	@Value("${agent.api.url}")
+	@Value("${agent.server.api.url}")
     private String agentApiUrl;    
 	
     @Autowired
